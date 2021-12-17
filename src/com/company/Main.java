98f1778762fdb4a38d7991d4b85950283001a357
kamleshkumarkohli <kamleshkumarkohli.cse2019@dscet.ac.in>
2021-12-17 22:31:33 +0530

    commiting try

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("tryong to connect with GitHub");
+	// write your code here
+    }
+}
---
+++ src/com/company/justtry.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class justtry {
+    public static void main(String[] args) {
+        System.out.println("trying to upload file into github");
+    }
+}
